0c62a6cccbfd3bbe23551eb8d8abcd0598972866
sutaminajing40 <sutaminajing40@gmail.com>
2023-01-07 23:40:49 +0900

    first commit

---
+++ spotify_id.py
@@ -0,0 +1,12 @@
+def id():
+    return client_id
+
+def secret():
+    return client_secret
+
+def user_name():
+    return user_id
+
+client_id = 'b9e3a248dffe4f25ad39434b0610f5cb'
+client_secret = 'ebf38ebc11c142d7aa30311a09e85e8e'
+user_id = 'nohoarito_yuzu_334129'
