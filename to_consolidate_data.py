882d855ad6ace632f77899d333cc5d17f947b11c
sutaminajing40 <sutaminajing40@gmail.com>
2023-03-05 14:54:13 +0900

    update
    それぞれのジャンルのパスを変更しなくていいようになりました。

--- to_consolidate_data.py
+++ to_consolidate_data.py
@@ -3,8 +3,18 @@ import glob
 import os
 
 # パスで指定したファイルの一覧をリスト形式で取得. （ここでは一階層下のtestファイル以下）
-genre = int(input('ジャンルを選択 1:邦ロック 2:女性アイドル 4:ボカロ 5:J-POP >>'))
-path = './csvfiles/Japanese_band'
+genre = int(input('ジャンルを選択 1:邦ロック 2:女性アイドル 3:歌い手 4:ボカロ 5:J-POP >>'))
+if genre == 1:
+    path = './csvfiles/Japanese_band'
+if genre == 2:
+    path = './csvfiles/girls_idol'
+if genre == 3:
+    path = './csvfiles/internet_singer'
+if genre == 4:
+    path = './csvfiles/vocaloid'
+if genre == 5:
+    path = './csvfiles/Jpop'
+
 csv_files = glob.glob(os.path.join(path,'*.csv'))
 
 #読み込むファイルのリストを表示
